f64ff23292743fd318a57c0687eea98f8edd88a7
steeeeeeeep <stephenedave.dorado@msugensan.edu.ph>
2024-04-24 18:45:44 +0800

    enhance dashboard

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -5,17 +5,118 @@ export default function Dashboard({ auth }) {
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">JAS</h2>}
         >
             <Head title="Dashboard" />
+                <div className="top p-6 text-gray-900">
+                    <div className="image">
+                        <img className='dashboard-images' src="https://img.freepik.com/free-photo/shop-clothing-clothes-shop-hanger-modern-shop-boutique_1150-8886.jpg?t=st=1713951090~exp=1713954690~hmac=ffef8b84df4f4a6fa12c278ae6f19feab299d692a34f3e88ecdc841f4b07ec9b&w=1060" alt='dashboard image'/>
+                    </div>
+                    
+                    <div class="categories p-6 grid grid-cols-6 gap-4">
+                        <div class="bg-gray-200 p-4">Column 1</div>
+                        <div class="bg-gray-300 p-4">Column 2</div>
+                        <div class="bg-gray-400 p-4">Column 3</div>
+                        <div class="bg-gray-500 p-4">Column 4</div>
+                        <div class="bg-gray-600 p-4">Column 5</div>
+                        <div class="bg-gray-700 p-4">Column 6</div>
+                        
+                        <div class="bg-gray-800 p-4">Column 7</div>
+                        <div class="bg-gray-900 p-4">Column 8</div>
+                        <div class="bg-gray-1000 p-4">Column 9</div>
+                        <div class="bg-gray-800 p-4">Column 10</div>
+                        <div class="bg-gray-900 p-4">Column 11</div>
+                        <div class="bg-gray-1000 p-4">Column 12</div>
+                    </div>
+                    
+                    <div class="p-6 grid grid-cols-1 gap-4">
+                        <div class="py-6">
+                            <div class="content-title">
+                                <div class="text-black-80 py-2">
+                                    <h1 class="uppercase bold text-center">Premium bags</h1>
+                                </div>
+                            </div>
+                            <div class="collections">
+                                <div class="grid grid-cols-6 gap-4">
+                                    <div class="bg-gray-200 p-2">Column 1</div>
+                                    <div class="bg-gray-300 p-2">Column 2</div>
+                                    <div class="bg-gray-400 p-2">Column 3</div>
+                                    <div class="bg-gray-500 p-2">Column 2</div>
+                                    <div class="bg-gray-600 p-2">Column 5</div>
+                                    <div class="bg-gray-700 p-2">Column 6</div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="py-6">
+                            <div class="content-title">
+                                <div class="text-black-80 py-2">
+                                    <h1 class="uppercase bold text-center">Women's Wear</h1>
+                                </div>
+                            </div>
+                            <div class="collections">
+                                <div class="grid grid-cols-6 gap-4">
+                                    <div class="bg-gray-200 p-2">Column 1</div>
+                                    <div class="bg-gray-300 p-2">Column 2</div>
+                                    <div class="bg-gray-400 p-2">Column 3</div>
+                                    <div class="bg-gray-500 p-2">Column 2</div>
+                                    <div class="bg-gray-600 p-2">Column 5</div>
+                                    <div class="bg-gray-700 p-2">Column 6</div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="py-6">
+                            <div class="content-title">
+                                <div class="text-black-80 py-2">
+                                    <h1 class="uppercase bold text-center">Men's wear</h1>
+                                </div>
+                            </div>
+                            <div class="collections">
+                                <div class="grid grid-cols-6 gap-4">
+                                    <div class="bg-gray-200 p-2">Column 1</div>
+                                    <div class="bg-gray-300 p-2">Column 2</div>
+                                    <div class="bg-gray-400 p-2">Column 3</div>
+                                    <div class="bg-gray-500 p-2">Column 2</div>
+                                    <div class="bg-gray-600 p-2">Column 5</div>
+                                    <div class="bg-gray-700 p-2">Column 6</div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="py-6">
+                            <div class="content-title">
+                                <div class="text-black-80 py-2">
+                                    <h1 class="uppercase bold text-center">Kid's wear</h1>
+                                </div>
+                            </div>
+                            <div class="collections">
+                                <div class="grid grid-cols-6 gap-4">
+                                    <div class="bg-gray-200 p-2">Column 1</div>
+                                    <div class="bg-gray-300 p-2">Column 2</div>
+                                    <div class="bg-gray-400 p-2">Column 3</div>
+                                    <div class="bg-gray-500 p-2">Column 2</div>
+                                    <div class="bg-gray-600 p-2">Column 5</div>
+                                    <div class="bg-gray-700 p-2">Column 6</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
+                    <div class="p-6 grid grid-cols-1 gap-4">
+                        <div class="py-6">
+                            <div class="collections">
+                                <div class="grid grid-cols-6 gap-4">
+                                    <div class="bg-gray-200 p-2">Column 1</div>
+                                    <div class="bg-gray-300 p-2">Column 2</div>
+                                    <div class="bg-gray-400 p-2">Column 3</div>
+                                    <div class="bg-gray-500 p-2">Column 2</div>
+                                    <div class="bg-gray-600 p-2">Column 5</div>
+                                    <div class="bg-gray-700 p-2">Column 6</div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
         </AuthenticatedLayout>
     );
 }
