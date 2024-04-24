1c80381048ca9c7ebfeafcf85d1fde69c0a89496
steeeeeeeep <stephenedave.dorado@msugensan.edu.ph>
2024-04-24 16:44:13 +0800

    Initial commit

---
+++ app/Models/Chirp.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use App\Events\ChirpCreated;
+use App\Events\NewEvent;
+use App\Notifications\NewChirp;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Chirp extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'message',
+    ];
+
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
